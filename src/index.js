14ba236bdf62896b8aab8da71fd40f0c0ea1f4e9
Félix Gagnon-Grenier <felix.gagnon-grenier@crim.ca>
2019-06-03 12:24:39 -0400

    following context recommendation

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@
 const {GIN_GraphQL} = require('./GIN_GraphQL');
 const Raven = require('raven');
 Raven.config('https://58d731f21594457087bbd41837a399f1@sentry.crim.ca/27').install();
-const gin_graph = new GIN_GraphQL();
-gin_graph.initialize();
+Raven.context(() => {
+    const gin_graph = new GIN_GraphQL();
+    gin_graph.initialize();
+});
